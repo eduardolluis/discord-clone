04235a509c6988dbbed6a3ea0169bf9c42b76f17
Eduardo De La Cruz <eduardodelacruzg4@gmail.com>
2025-10-22 16:04:05 -0400

    Navigation sidebar

---
+++ components/navigation/navigation-action.tsx
@@ -0,0 +1,21 @@
+"use client";
+import { PlusIcon } from "lucide-react";
+
+import { ActionTooltip } from "@/components/action-tooltip";
+
+export const NavigationAction = () => {
+  return (
+    <div>
+      <ActionTooltip label="Add a server" side="right" align="center">
+        <button className="group flex items-center">
+          <div className="flex mx-3 h-12 w-12 rounded-3xl group-hover:rounded-2xl transition-all overflow-hidden items-center justify-center bg-neutral-700 light:bg-white-700 group-hover:bg-emerald-500 cursor-pointer">
+            <PlusIcon
+              className="group-hover:text-white transition text-emerald-500"
+              size={25}
+            />
+          </div>
+        </button>
+      </ActionTooltip>
+    </div>
+  );
+};
