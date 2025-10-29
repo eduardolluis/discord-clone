c2c93cab69ece517f83ec338cc7103e1b6cf4781
Eduardo De La Cruz <eduardodelacruzg4@gmail.com>
2025-10-29 12:01:39 -0400

    Chat Header

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,5 +1,50 @@
-const ChannelIdPage = () => {
-  return <div>ChannelIdPage</div>;
+import { ChatHeader } from "@/components/chat/chat-header";
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { redirect } from "next/navigation";
+
+interface ChannelIdPageProps {
+  params: Promise<{
+    serverId: string;
+    channelId: string;
+  }>;
+}
+
+const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
+  const profile = await currentProfile();
+  
+  if (!profile) {
+    return redirect("/sign-in");
+  }
+
+  const { serverId, channelId } = await params;
+
+  const channel = await db.channel.findUnique({
+    where: {
+      id: channelId,
+    },
+  });
+
+  const member = await db.member.findFirst({
+    where: {
+      serverId: serverId,
+      profileId: profile.id,
+    },
+  });
+
+  if (!channel || !member) {
+    return redirect("/");
+  }
+
+  return (
+    <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
+      <ChatHeader 
+        serverId={serverId}
+        name={channel.name}
+        type="channel"
+      />
+    </div>
+  );
 };
 
 export default ChannelIdPage;
