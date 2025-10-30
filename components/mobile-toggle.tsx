6cf30381f69a8a9b8eb04000c9ff618c37cc789a
Eduardo De La Cruz <eduardodelacruzg4@gmail.com>
2025-10-30 09:54:23 -0400

    fixed(ServerSidebarMobile view)

--- components/mobile-toggle.tsx
+++ components/mobile-toggle.tsx
@@ -1,15 +1,10 @@
 import { Menu } from "lucide-react";
-
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { Button } from "@/components/ui/button";
 import { NavigationSidebar } from "@/components/navigation/navigation-sidebar";
 import { ServerSidebar } from "@/components/server/server-sidebar";
 
-export const MobileToggle = ({
-    serverId
-}: {
-    serverId: string;
-}) => {
+export const MobileToggle = ({ serverId }: { serverId: string }) => {
   return (
     <Sheet>
       <SheetTrigger asChild>
@@ -17,11 +12,15 @@ export const MobileToggle = ({
           <Menu />
         </Button>
       </SheetTrigger>
-      <SheetContent side="left" className="p-0 flex gap-0">
-        <div className="w-[72px]">
-          <NavigationSidebar />
+      <SheetContent side="left" className="p-0 w-[320px] sm:w-[400px]">
+        <div className="flex h-full">
+          <div className="w-[72px] bg-[#1E1F22]">
+            <NavigationSidebar />
+          </div>
+          <div className="flex-1 bg-[#2B2D31]">
+            <ServerSidebar serverId={serverId} />
+          </div>
         </div>
-        <ServerSidebar serverId={serverId} />
       </SheetContent>
     </Sheet>
   );
