b6d6757ab3603d7a1360a8c52832974a86abfffa
Eduardo De La Cruz <eduardodelacruzg4@gmail.com>
2025-11-02 19:11:33 -0400

    Chat Messages component

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import { ModalProvider } from "@/components/providers/modal-provider";
 import { SocketProvider } from "@/components/providers/socket-provider";
+import { QueryProvider } from "@/components/providers/query-provider";
 
 const font = Open_Sans({
   variable: "--font-open-sans",
@@ -32,9 +33,9 @@ export default function RootLayout({
             enableSystem={false}
             storageKey="discordia-theme"
           >
-            <SocketProvider>  
+            <SocketProvider>
               <ModalProvider />
-              {children}
+              <QueryProvider>{children}</QueryProvider>
             </SocketProvider>
           </ThemeProvider>
         </body>
